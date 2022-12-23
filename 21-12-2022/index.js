76cf324a0f6c03a263c6b88221465c8548e586d5
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2022-12-23 12:31:12 +0500

    21-12-2022

---
+++ 21-12-2022/index.js
@@ -0,0 +1,96 @@
+// function add(currBalance,amount){
+//     let result=parseFloat(currBalance)+parseFloat(amount)
+//     return result
+// }
+
+// let deposit1=add(35000,5000)
+// document.write("New Balance After Deposit 1 is: "+deposit1)
+// document.write("<br>")
+// let deposit2=add(40000,15000)
+// document.write("New Balance After Deposit 2 is: "+deposit2)
+// document.write("<br>")
+// let deposit3=add(55000,25000)
+// document.write("New Balance After Deposit 3 is: "+deposit3)
+
+// document.write("<br>")
+
+// let transfer1=add(35000,2500)
+// document.write("New Balance After Transfer 1 is: "+transfer1)
+// document.write("<br>")
+// let transfer2=add(40000,1000)
+// document.write("New Balance After Transfer 2 is: "+transfer2)
+// document.write("<br>")
+// let transfer3=add(55000,250000)
+// document.write("New Balance After Transfer 3 is: "+transfer3)
+
+
+// let car1={
+//     name:"Mehran",
+//     color:"Black",
+//     number:"123",
+//     model:"1998",
+//     transmission:"manual"
+// }
+
+// let car2={
+//     name:"Cultus",
+//     color:"Silver",
+//     number:"146",
+//     model:"2012",
+//     transmission:"manual"
+// }
+// document.write(car1.name+" "+car1.color+" "+car1.number+" "+car1.model+" "+car1.transmission)
+// document.write("<br>")
+// document.write(car2.name+" "+car2.color+" "+car2.number+" "+car2.model+" "+car2.transmission)
+
+// let parallelogram1={
+//     base:2,
+//     height:5
+// }
+
+// function area(b,h){
+// let area=b*h
+// return area
+// }
+
+// let answer=area(parallelogram1.base,parallelogram1.height)
+// document.write(answer)
+
+// let square={
+//     radius:5,
+//     area:function(rad){
+// let areaofCircle=3.142*(rad*rad)
+// document.write(areaofCircle)
+//     }
+
+// }
+
+// square.area(square.radius)
+
+// let today=new Date()
+// let month=today.getMonth()
+// switch(month){
+//     case 0:
+//         month="January"
+//         break
+//     case 11:
+//         month="December"
+//         break
+//     default:
+//         alert("Invalid")
+// }
+// let date=today.getDate()
+// let year=today.getFullYear()
+// document.write(date+" "+month+" ,"+year)
+
+// let today=new Date()
+// document.write(today.getSeconds())
+// let data=Math.trunc(4.55689)
+// document.write(data)
+
+// let sentence="Hello World"
+// document.write(sentence.indexOf("o"))
+
+// let sentence="Hello World"
+// document.write(sentence.toUpperCase())
+
